97a033cf06a3a4a8b38d49d31fa6f4b98a785a67
Kolya <kolya.romanov8@gmail.com>
2020-10-02 12:12:08 +0300

    Working mvc+thymeleaf hello world version

--- src/main/java/com/foxminded/university/config/ApplicationConfig.java
+++ src/main/java/com/foxminded/university/config/ApplicationConfig.java
@@ -12,7 +12,7 @@ import javax.sql.DataSource;
 import java.util.Objects;
 
 @Configuration
-@ComponentScan("com.foxminded.university")
+@ComponentScan("com.foxminded.university.dao")
 @PropertySource("classpath:config.properties")
 public class ApplicationConfig {
 
